a88a675197e49a35034d6b1a065e93a8ac5a11ba
Nguyễn Bình An <167419245+TomOutfit@users.noreply.github.com>
2025-10-14 20:46:40 +0700

    Model

---
+++ FUMiniHotelSystem.Models/AppSettings.cs
@@ -0,0 +1,9 @@
+namespace FUMiniHotelSystem.Models
+{
+    public class AppSettings
+    {
+        public string AdminEmail { get; set; } = "admin@FUMiniHotelSystem.com";
+        public string AdminPassword { get; set; } = "@@abc123@@";
+        public string DataPath { get; set; } = "Data";
+    }
+}
